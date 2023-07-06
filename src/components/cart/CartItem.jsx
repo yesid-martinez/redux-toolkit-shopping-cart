49282dbb2a76361bcd4f639e1178a99aef3c82db
Yesid Gomez <wujibifan.hh@gmail.com>
2023-07-06 11:29:42 -0500

    SCSS - CartContainer, CartItem y CartFooter

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types';
 
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+
 const CartItem = ({id, title,price, img, amount}) => {
     
-    console.log(id);
-    
     return (
-        <article>
+        <article key={id} className="cart-item">
             <img src={img} alt={title} />
             <div>
-                <h3>{price}</h3>
-                <p>${price}</p>
-                <p>{amount}</p>
+                <h4>{title}</h4>
+                <h4 className="item-price">${price}</h4>
+                <button className="remove-btn">Remove</button>
+            </div>
+            <div>
+                <button className="amount-btn">
+                    <FaChevronUp/>
+                </button>
+                <p className="amount">{amount}</p>
+                <button className="amount-btn">
+                    <FaChevronDown/>
+                </button>
             </div>
         </article>
     )
